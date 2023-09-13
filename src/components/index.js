452e661e4a1e327621b009b5aa54d35191213d80
AgusDonalisio <64385057+dazxon@users.noreply.github.com>
2023-09-13 18:23:19 -0300

    What I Do component created
    Minor components that What I Do component renders also created

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import { Navbar } from "./Navbar";
 import { Hello } from "./Hello";
 import { AboutMe } from "./AboutMe";
 import { TempModal } from "./TempModal";
+import { WhatIDo } from "./WhatIDo";
+import { WhatIDoItem } from "./WhatIDoItem";
 
-export { Navbar, Hello, AboutMe, TempModal };
+export { Navbar, Hello, AboutMe, TempModal, WhatIDo, WhatIDoItem };
